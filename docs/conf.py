e8604965ad069870afdbb3041865cffdd2290d87
wojtiwojt <wojti.wojt91@gmail.com>
2021-08-03 14:08:29 +0200

    test github pages

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Digital-Desire'
-copyright = '2021, Digital-Desire'
-author = 'Digital-Desire'
+project = 'Digital-desire'
+copyright = '2021, Wojti'
+author = 'Wojti'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -45,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
