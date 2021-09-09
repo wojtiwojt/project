8abe5b2e3d5f530ee320b8a7f01b9195bc791f6d
wojtiwojt <wojti.wojt91@gmail.com>
2021-09-09 15:51:57 +0200

    custom user model

---
+++ app/backend/newsapp/users/models.py
@@ -0,0 +1,22 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class User(AbstractUser):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    username = None
+    email = models.EmailField(_("Email adress"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
