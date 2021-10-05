ccac23fc2ebe4abe1d4063037eb4af2eea947123
wojtiwojt <wojti.wojt91@gmail.com>
2021-10-05 15:48:40 +0200

    change password when logged in

--- app/backend/newsapp/users/urls.py
+++ app/backend/newsapp/users/urls.py
@@ -6,6 +6,8 @@ from django.contrib.auth.views import (
     PasswordResetDoneView,
     PasswordResetConfirmView,
     PasswordResetCompleteView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
 )
 
 
@@ -35,4 +37,14 @@ urlpatterns = [
         name="password_reset_complete",
     ),
     path("password_reset", password_reset_request, name="password_reset"),
+    path(
+        "password_change/",
+        PasswordChangeView.as_view(template_name="users/password_change.html"),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"),
+        name="password_change_done",
+    ),
 ]
