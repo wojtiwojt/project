b8122a22aeb992e6639c4af35d7820df9f6f8f2d
wojtiwojt <wojti.wojt91@gmail.com>
2021-10-05 13:35:36 +0200

    account recovery with email link

--- app/backend/newsapp/users/urls.py
+++ app/backend/newsapp/users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import UserRegistrationView, ActivateUserAccount
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import UserRegistrationView, ActivateUserAccount, password_reset_request
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
 
 urlpatterns = [
@@ -9,4 +15,24 @@ urlpatterns = [
     path("activate/<uidb64>/<token>/", ActivateUserAccount.as_view(), name="activate"),
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="base.html"), name="logout"),
+    path(
+        "password_reset/done/",
+        PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "password_reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password_reset/success/",
+        PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+    path("password_reset", password_reset_request, name="password_reset"),
 ]
