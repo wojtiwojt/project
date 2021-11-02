564153ed9229f90ade0a3097fbce5214dfc1f024
Tomasz Czyż <tomasz.czyz@eskapizm.pl>
2021-11-02 18:06:34 +0100

    move files (WIP, refactor needed)

--- backend/core/urls/api.py
+++
@@ -1,5 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path("v1/", include("core.urls.v1")),
-]
--- backend/core/urls/v1.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path("hj6&a/", admin.site.urls),
-]
