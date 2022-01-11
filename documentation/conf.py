81d6e4dbe0af81f0d3b2de650243b25a0f3f7d1a
Tomasz Czyż <tomasz.czyz@eskapizm.pl>
2022-01-11 23:56:13 +0100

    remove autobuild from sphinx extension list (not required to be there)

--- documentation/conf.py
+++ documentation/conf.py
@@ -1,10 +1,10 @@
-project = 'pozabańką.pl'
+project = 'pozabanka.pl'
 copyright = '2021, poza bańką'
-author = 'pozabańką.pl team'
+author = 'pozabanka.pl team'
 release = 'no-version'
 extensions = [
-    "sphinxcontrib.needs"
-    "sphinxcontrib.autobuild"
+    "sphinxcontrib.needs",
+#    "sphinxcontrib.autobuild"
 ]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
